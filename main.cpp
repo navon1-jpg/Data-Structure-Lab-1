b6008473c949b3cadeecf5ae7df40859d401828e
navon1-jpg <navonmoreland1@gmail.com>
2026-02-05 09:46:55 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,42 +1,28 @@
 //
 //  main.cpp
-//  Lab 1 Data Strc
+//  Die.cpp
 //
 //  Created by Navon M on 1/15/26.
-//
 
-#include <iostream>
+
 #include "Die.h"
+#include <cstdlib>
 
-int main()
-{
-    Die d6;          // default: 6-sided
-    Die d20(20);     // 20-sided (valid)
-    Die badDie(1);   // invalid => defaults to 6
+Die::Die() {
+    sides = 6;
+}
 
-    std::cout << "d6 sides: " << d6.sides()
-              << ", current value: " << d6.value() << "\n";
+Die::Die(int s) {
+    if (s >= 2)
+        sides = s;
+    else
+        sides = 6;
+}
 
-    std::cout << "Rolling d6 five times:\n";
-    for (int i = 0; i < 5; ++i)
-    {
-        int v = d6.roll();
-        std::cout << " roll " << (i + 1) << ": " << v << "\n";
-    }
+int Die::roll() {
+    return rand() % sides + 1;
+}
 
-    std::cout << "\nd20 sides: " << d20.sides() << "\n";
-    std::cout << "Rolling d20 three times:\n";
-    for (int i = 0; i < 3; ++i)
-    {
-        std::cout << " " << d20.roll() << "\n";
-    }
-
-    std::cout << "\n'badDie' sides (should be 6): "
-              << badDie.sides() << "\n";
-
-    std::cout << "Rolling badDie twice:\n";
-    std::cout << " " << badDie.roll() << "\n";
-    std::cout << " " << badDie.roll() << "\n";
-
-    return 0;
+int Die::getSides() const {
+    return sides;
 }
